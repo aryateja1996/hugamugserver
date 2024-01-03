cd673d2aed1c0ba2661788a7cff563e0a7af9f32
aryateja1996 <97657384+SreekarKondaveeti@users.noreply.github.com>
2024-01-03 11:54:53 +0530

    delete count

--- index.js
+++ index.js
@@ -39,6 +39,16 @@ const category = [
   "thickshakes",
   "icecream",
 ];
+
+app.delete('/',async(req,res)=>{
+  var countsmain =  await firebaseDb.collection('dashboard').doc('counts').get();
+ var counts = countsmain.data()
+ for (let i = 0; i < category.length; i++) {
+  var key = category[i] + "Orders"
+    counts[key] = 0
+}
+res.send("Deleted")
+})
 //List Here
 app.get('/menu/:type?', async (req, res) => {
   console.log("menu");
@@ -72,12 +82,20 @@ app.get('/menu/:type?', async (req, res) => {
   }
  }
 });
-
+const name = 'mug'
+app.post('/login',async(req,res)=>{
+  if (req.body["name"].toLowerCase() == name || req.body["name"].toLowerCase() == 'veda') {
+    res.sendStatus(200)
+  } else {
+    res.sendStatus(400)
+  }
+})
 app.post('/order',async(req,res)=>{
-  reqBody = req.body;
-  console.log(req.body);
+ 
   const date = new Date()
  var finalDate = `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`
+ var dateToday = `${date.toDateString()}`
+ console.log(dateToday,"Date Today");
   var data = {}
   data = {
     "date": finalDate,
@@ -87,7 +105,6 @@ app.post('/order',async(req,res)=>{
     "itemName": req.body["itemName"],
     "paymentMode":req.body["paymentMode"]
   }
-  console.log(data);
  var countsmain =  await firebaseDb.collection('dashboard').doc('counts').get();
  var counts = countsmain.data()
 if(data["paymentMode"] != null){
@@ -103,9 +120,23 @@ for (let i = 0; i < category.length; i++) {
     counts[key] = counts[key]+data['quantity']
   }
 }
+var orders = []
+await firebaseDb.collection('order').doc(dateToday.toLocaleUpperCase()).collection('todayOrders').get()
+    .then(querySnapshot => {
+        querySnapshot.docs.map(doc => {
+            // console.log('LOG 1', doc.data());
+            orders.push(doc.data())
+            // console.log("Oredrs", orders);
+            return doc.data();
+        });
+    });
+// console.log(orders , "Orders Length")
+var docId = orders.length + 1
+// console.log(docId, "Document Id ::")
 // Order Collection Population
-  await firebaseDb.collection('order').doc().set(data).then(async()=>{
+  await firebaseDb.collection('order').doc(dateToday.toLocaleUpperCase()).collection('todayOrders').doc(docId.toString()).set(data).then(async()=>{
     
+// console.log(docId, "Document Id :: Inside")
     counts['totalOrders'] = counts["totalOrders"] + 1;
     //Dashboard counts Population
     await firebaseDb.collection('dashboard').doc('counts').set(counts)
